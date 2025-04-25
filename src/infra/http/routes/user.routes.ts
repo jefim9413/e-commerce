c3f4a3cb1ccf08708a7146da3390ca14a7cf9731
Jefferson Marques <56725964+jefim9413@users.noreply.github.com>
2025-04-25 00:52:25 -0300

    feat: adicionando rota para getUserProfile

--- src/infra/http/routes/user.routes.ts
+++ src/infra/http/routes/user.routes.ts
@@ -2,10 +2,14 @@ import { FastifyInstance } from 'fastify'
 import { createUser } from '../controllers/create-user'
 import { refresh } from '../controllers/refresh'
 import { authenticate } from '../controllers/authenticate'
+import { verifyJwt } from '../middlewares/verify-jwt'
+import { getProfile } from '../controllers/profile'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', createUser)
   app.post('/sessions', authenticate)
 
   app.patch('/token/refresh', refresh)
+
+  app.get('/me', { onRequest: [verifyJwt] }, getProfile)
 }
