a60bccbe1cbb47cf4f185521922eabcd64cb9ad9
Jefferson Marques <56725964+jefim9413@users.noreply.github.com>
2025-05-02 16:51:11 -0300

    feat: adicionando rota de get produto

--- src/infra/http/routes/product.routes.ts
+++ src/infra/http/routes/product.routes.ts
@@ -4,9 +4,11 @@ import { verifyUserRole } from '../middlewares/verify-user-role'
 import { createProduct } from '../controllers/create-product'
 import { listProducts } from '../controllers/list-products'
 import { removeProduct } from '../controllers/remove-product'
+import { getProduct } from '../controllers/get-product'
 
 export async function productRoutes(app: FastifyInstance) {
   app.get('/products', listProducts)
+  app.get('/products/:id', getProduct)
 
   app.register(async (privateRoutes) => {
     privateRoutes.addHook('onRequest', verifyJwt)
