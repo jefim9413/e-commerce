13414f09761c47dae1a4c3e11fe81fbf7022c63c
Jefferson Marques <56725964+jefim9413@users.noreply.github.com>
2025-05-14 08:20:47 -0300

    feat: adicionando teste unitario de adicionar produto ao carinho

---
+++ src/core/repositories/in-memory/in-memory-cart-repository.ts
@@ -0,0 +1,39 @@
+import {
+  CartRepository,
+  CreateCartDTO,
+} from '@/core/repositories/cart-repository'
+import { Cart } from '@prisma/client'
+import { randomUUID } from 'crypto'
+
+export class InMemoryCartRepository implements CartRepository {
+  public items: Cart[] = []
+
+  async create(data: CreateCartDTO): Promise<Cart> {
+    const cart: Cart = {
+      id: randomUUID(),
+      createdAt: new Date(),
+      ...data,
+    }
+
+    this.items.push(cart)
+    return cart
+  }
+
+  async findByUserAndProduct(
+    userId: string,
+    productId: string,
+  ): Promise<Cart | null> {
+    return (
+      this.items.find(
+        (item) => item.userId === userId && item.productId === productId,
+      ) ?? null
+    )
+  }
+
+  async save(cart: Cart): Promise<void> {
+    const index = this.items.findIndex((item) => item.id === cart.id)
+    if (index >= 0) {
+      this.items[index] = cart
+    }
+  }
+}
