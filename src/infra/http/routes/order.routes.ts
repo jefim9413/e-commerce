bdb6119decacbebcfeb1914b54b488617793a061
Jefferson Marques <56725964+jefim9413@users.noreply.github.com>
2025-05-22 16:24:54 -0300

    feat: adicionando rota de get orders

--- src/infra/http/routes/order.routes.ts
+++ src/infra/http/routes/order.routes.ts
@@ -1,9 +1,11 @@
 import { FastifyInstance } from 'fastify'
 import { verifyJwt } from '../middlewares/verify-jwt'
 import { createOrder } from '../controllers/create-order'
+import { listUserOrders } from '../controllers/list-user-orders'
 
 export async function orderRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJwt)
 
   app.post('/checkout', createOrder)
+  app.get('/orders', listUserOrders)
 }
