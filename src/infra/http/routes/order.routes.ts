9bbcc3fd95c29a758b79ad538cc5a4e23acab6c3
Jefferson Marques <56725964+jefim9413@users.noreply.github.com>
2025-07-03 17:21:36 -0300

    feat: adicionando rota para order details

--- src/infra/http/routes/order.routes.ts
+++ src/infra/http/routes/order.routes.ts
@@ -2,10 +2,13 @@ import { FastifyInstance } from 'fastify'
 import { verifyJwt } from '../middlewares/verify-jwt'
 import { createOrder } from '../controllers/create-order'
 import { listUserOrders } from '../controllers/list-user-orders'
+import { getOrderDetails } from '../controllers/get-order-details'
 
 export async function orderRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJwt)
 
   app.post('/checkout', createOrder)
   app.get('/orders', listUserOrders)
+
+  app.get('/orders/:id', getOrderDetails)
 }
