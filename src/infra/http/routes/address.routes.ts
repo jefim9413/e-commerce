f9b8c96b069a5a504539ef4ecb97d093d67dc6bd
Jefferson Marques <56725964+jefim9413@users.noreply.github.com>
2025-07-29 23:39:29 -0300

    feat: adicionando rota de list address

--- src/infra/http/routes/address.routes.ts
+++ src/infra/http/routes/address.routes.ts
@@ -1,9 +1,11 @@
 import { FastifyInstance } from 'fastify'
 import { verifyJwt } from '../middlewares/verify-jwt'
 import { createAddress } from '../controllers/create-address'
+import { listUserAddress } from '../controllers/list-address'
 
 export async function addressRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJwt)
 
   app.post('/addresses', createAddress)
+  app.get('/addresses', listUserAddress)
 }
