d31703ec4f8de6c1426f2261e934ac0559102fab
Jefferson Marques <56725964+jefim9413@users.noreply.github.com>
2025-08-05 22:33:50 -0300

    feat: adicionando rotas de address

--- src/infra/http/routes/address.routes.ts
+++ src/infra/http/routes/address.routes.ts
@@ -2,10 +2,16 @@ import { FastifyInstance } from 'fastify'
 import { verifyJwt } from '../middlewares/verify-jwt'
 import { createAddress } from '../controllers/create-address'
 import { listUserAddress } from '../controllers/list-address'
+import { getAddressDetails } from '../controllers/get-address-details'
+import { updateAddress } from '../controllers/update-address'
+import { removeAddress } from '../controllers/remove-address'
 
 export async function addressRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJwt)
 
   app.post('/addresses', createAddress)
   app.get('/addresses', listUserAddress)
+  app.get('/addresses/:addressId', getAddressDetails)
+  app.patch('/addresses/:addressId', updateAddress)
+  app.delete('/addresses/:addressId', removeAddress)
 }
