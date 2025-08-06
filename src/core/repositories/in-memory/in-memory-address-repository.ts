75183875f24979b45c8c91e49206238fb42df9b3
Jefferson Marques <56725964+jefim9413@users.noreply.github.com>
2025-08-05 22:35:05 -0300

    feat: adicionando repository in-memory

--- src/core/repositories/in-memory/in-memory-address-repository.ts
+++ src/core/repositories/in-memory/in-memory-address-repository.ts
@@ -1,5 +1,5 @@
 import { AddressRepository } from '@/core/repositories/address-repository'
-import { Address } from '@prisma/client'
+import { Address, Prisma } from '@prisma/client'
 
 function randomId() {
   return Math.random().toString(36).substring(2)
@@ -24,5 +24,28 @@ export class InMemoryAddressRepository implements AddressRepository {
 
   async findById(id: string): Promise<Address | null> {
     return this.addresses.find((addr) => addr.id === id) || null
+  }
+
+  async update(id: string, data: Prisma.AddressUpdateInput): Promise<Address> {
+    const index = this.addresses.findIndex((a) => a.id === id)
+    if (index === -1) throw new Error('Address not found')
+    this.addresses[index] = {
+      ...this.addresses[index],
+      ...Object.fromEntries(
+        Object.entries(data).map(([key, value]) => [
+          key,
+          typeof value === 'object' && value !== null && 'set' in value
+            ? value.set
+            : value,
+        ]),
+      ),
+    }
+    return this.addresses[index]
+  }
+
+  async remove(id: string): Promise<void> {
+    const index = this.addresses.findIndex((a) => a.id === id)
+    if (index === -1) throw new Error('Address not found')
+    this.addresses.splice(index, 1)
   }
 }
