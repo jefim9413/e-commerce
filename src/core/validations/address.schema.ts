6d6b4df4faee135216c1682df8c229f30a90d659
Jefferson Marques <56725964+jefim9413@users.noreply.github.com>
2025-08-05 22:40:18 -0300

    feat: adicionando validations para address

--- src/core/validations/address.schema.ts
+++ src/core/validations/address.schema.ts
@@ -8,5 +8,15 @@ export const createAddressSchema = z.object({
   zipcode: z.string().min(8, 'CEP deve ter pelo menos 8 caracteres'),
   complement: z.string().optional(),
 })
+export const updateAddressSchema = z.object({
+  street: z.string().min(1, 'Rua obrigatória').optional(),
+  number: z.string().min(1, 'Número obrigatório').optional(),
+  city: z.string().min(1, 'Cidade obrigatória').optional(),
+  state: z.string().min(1, 'Estado obrigatório').optional(),
+  zipcode: z.string().min(1, 'CEP obrigatório').optional(),
+  complement: z.string().optional(),
+})
+
+export type UpdateAddressDTO = z.infer<typeof updateAddressSchema>
 
 export type CreateAddressDTO = z.infer<typeof createAddressSchema>
